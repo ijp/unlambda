30db79e709d4c316684ebc59ae50b2ba0a1367c8
Ian Price <ianprice90@googlemail.com>
2013-04-15 22:14:57 +0100

    Add first attempt at interpreter

--- unlambda.lua
+++ unlambda.lua
@@ -1,3 +1,105 @@
+-- Warning: The primitives of the Unlambda interpreter are curried and
+-- written in Continuation Passing Style to accommodate Unlambda's 'c'
+-- primitive. If you haven't seen code written like this before, don't
+-- hate me, usually you'd have the machine do the converting for you.
+
+function k (x, cont)
+   local function inner (y, cont)
+      return cont(x)
+   end
+   return cont(inner)
+end
+
+-- this procedure is quite disgusting, c'est la vie
+function s (f, cont)
+   local function inner (g, cont)
+      local function inner2 (x, cont)
+         local function fcont(f)
+            local function gcont(a)
+               return f(a, cont)
+            end
+            return g(x, gcont)
+         end
+         return f(x, fcont);
+      end
+      return cont(inner2)
+   end
+   return cont(inner)
+end
+
+function i (x, cont)
+   return cont(x)
+end
+
+function void (x, cont)
+   return cont(v)
+end
+
+function callcc (f, cont)
+   local function inner (x, cont2)
+      return cont(x)
+   end
+   return f(inner, cont)
+end
+
+function display (c)
+   local function inner(x, cont)
+      io.write(c)
+      return cont(x)
+   end
+   return inner
+end
+
+function delay(expr, cont)
+   function new(arg,cont)
+      function econt (f)
+         return f(arg,cont)
+      end
+      return eval(expr, econt)
+   end
+   return cont(new)
+end
+
+-- Interpreter
+
+function evapply(f, a, cont)
+   local function fcont(fval)
+      local function acont(aval)
+         return fval(aval, cont)
+      end
+      if type(fval) == 'function' then
+         return eval(a, acont);
+      elseif type(fval) == 'table' and fval.type == 'delay' then
+         return delay(a,cont);
+      else
+         error()
+      end
+   end
+   return eval(f, fcont)
+end
+
+function eval(tree, cont)
+   if tree.type == 'apply' then
+      return evapply(tree.func, tree.arg, cont);
+   elseif tree.type == 's' then
+      return cont(s);
+   elseif tree.type == 'k' then
+      return cont(k);
+   elseif tree.type == 'i' then
+      return cont(i);
+   elseif tree.type == 'v' then
+      return cont(void)
+   elseif tree.type == 'print' then
+      return cont(display(tree.c));
+   elseif tree.type == 'delay' then
+      return cont(tree);
+   elseif tree.type == 'c' then
+      return cont(callcc);
+   else
+      error()
+   end
+end
+
 function parse(file)
    while true do
       local char = file:read(1)
@@ -23,7 +125,7 @@ function parse(file)
             error()
          end
       elseif char == 'D' or char == 'd' then
-         return {type='d'}
+         return {type='delay'}
       elseif char == 'C' or char == 'c' then
          return {type='c'}
       elseif char == 'R' or char == 'r' then
@@ -46,8 +148,13 @@ function dump(tree)
    end
 end
 
+function id(x)
+   return x
+end
+
 function main(desc)
-   dump(parse(desc))
+   --dump(parse(desc))
+   eval(parse(desc),id)
 end
 
 if arg[1] then
